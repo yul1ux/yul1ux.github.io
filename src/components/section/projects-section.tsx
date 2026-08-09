b4c6da7778bb5ce79263c26fce508b877c25cfad
Soe Thiha <58359925+yul1ux@users.noreply.github.com>
2026-08-09 21:28:51 +0630

    Adjust grid layout width in projects section

--- src/components/section/projects-section.tsx
+++ src/components/section/projects-section.tsx
@@ -29,7 +29,7 @@ export default function ProjectsSection() {
                         </p>
                     </div>
                 </div>
-                <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto auto-rows-fr">
+                <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 max-w-[900px] mx-auto auto-rows-fr">
                     {DATA.projects.map((project, id) => (
                         <BlurFade
                             key={project.title}
